dde433802cf6eecd5bf5065e5127d257f2b6b26a
magg666 <mw23127@gmail.com>
2019-03-27 14:26:55 +0100

    initial commit

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-
+@app.route('/')
 @app.route('/index')
 def home():
  return render_template('index.html')
@@ -13,5 +13,5 @@ def home():
 def sw():
     return app.send_static_file('sw.js')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+app.run(debug=True)
