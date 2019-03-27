f46f7bfca57f3eeec78b973631ba6a27cf2f9765
magg666 <mw23127@gmail.com>
2019-03-27 14:17:44 +0100

    initial commit

--- server.py
+++ server.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/index')
+def home():
+ return render_template('index.html')
 
 
-@app.route('/service-worker.js', methods=['GET'])
+@app.route('/sw.js', methods=['GET'])
 def sw():
-    return app.send_static_file('service-worker.js')
+    return app.send_static_file('sw.js')
 
 if __name__ == '__main__':
     app.run(debug=True)
