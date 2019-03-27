ed204825ba90fdbae1636432480939039b6edbfc
magg666 <mw23127@gmail.com>
2019-03-27 14:09:58 +0100

    initial commit

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/service-worker.js', methods=['GET'])
+def sw():
+    return app.send_static_file('service-worker.js')
 
 if __name__ == '__main__':
     app.run(debug=True)
