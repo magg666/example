219a3cb429e9d9c5ca50e52b5fea71e4096d03b6
magg666 <mw23127@gmail.com>
2019-03-27 11:41:57 +0100

    initial commit

---
+++ static/js/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'wpisz-tutaj-dowolny-string';
+
+// List of files which are store in cache.
+let filesToCache = [
+    '/index.html',
+    '/hello-world.css',
+    '/main.js'
+];
+
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(filesToCache);
+        }).catch(function (err) {
+            // Snooze errors...
+            // console.error(err);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (evt) {
+    // Snooze logs...
+    // console.log(event.request.url);
+    evt.respondWith(
+        // Firstly, send request..
+        fetch(evt.request).catch(function () {
+            // When request failed, return file from cache...
+            return caches.match(evt.request);
+        })
+    );
+});
