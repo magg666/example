07afcfd3361a68103810ecf7e0788f31357b4146
magg666 <mw23127@gmail.com>
2019-03-27 14:27:13 +0100

    initial commit

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def home():
- return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/sw.js', methods=['GET'])
